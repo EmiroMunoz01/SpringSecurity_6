b15110e1b3c8a73f0f5ac8b39f50d18327794536
EmiroMunoz01 <emiro_2001@icloud.com>
2025-01-26 16:03:49 -0500

    Se ha creado el componente encargado de la autenticacion, se ha importado he inyectado, pero tambien podriamos usarlo pasandolo como parametros en la funcion del authenticationManager y podremos usarlo de igual forma

--- src/main/java/com/security/springsecurity_6/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurity_6/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.security.springsecurity_6.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,10 +17,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     //in this place I create the configuration, include the filters and more things
 
+    //2 inyectamos el componente que vamos a utilizar
+    @Autowired
+    AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.build();
     }
 
+
+    //1 este componente lo creamos a partir de un objeto que ya existe en spring security, que es Auth
+    public AuthenticationManager authenticationManager() throws Exception {
+
+        //3 con esto estamos configurando el componente encargado de la autenticacion
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
 }
